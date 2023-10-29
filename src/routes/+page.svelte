9d2cea653abbdb81802b8062ca5e27923ff5edac
bencohendev <ben@bencohen.dev>
2023-10-29 16:59:16 -0700

    tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <!-- src/routes/+page.svelte -->
 <script lang="ts">
 	import { Auth } from '@supabase/auth-ui-svelte';
-	import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 	export let data;
 </script>
@@ -10,14 +9,13 @@
 	<title>User Management</title>
 </svelte:head>
 
-<div class="row flex-center flex">
-	<div class="col-6 form-widget">
+<div>
+	<div>
 		<Auth
 			supabaseClient={data.supabase}
 			view="magic_link"
 			redirectTo={`${data.url}/auth/callback`}
 			showLinks={false}
-			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
 		/>
 	</div>
 </div>
